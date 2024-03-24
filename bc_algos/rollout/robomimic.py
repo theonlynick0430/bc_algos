b30dca9818b1e220d027ec894c2214394c679077
Nikhil Sridhar <niksrid@umich.edu>
2024-03-24 15:12:02 -0400

    better inheritance for rollout

--- bc_algos/rollout/robomimic.py
+++ bc_algos/rollout/robomimic.py
@@ -33,18 +33,16 @@ class RobomimicRolloutEnv(RolloutEnv):
 
             render_video (bool): whether to render rollout on screen
         """
+        assert isinstance(validset, RobomimicDataset)
+
         super(RobomimicRolloutEnv, self).__init__(
+            validset=validset,
             obs_group_to_key=obs_group_to_key,
             obs_key_to_modality=obs_key_to_modality,
             frame_stack=frame_stack,
             gc=gc,
             render_video=render_video,
         )
-
-        assert isinstance(validset, RobomimicDataset)
-        assert validset.pad_frame_stack and validset.pad_seq_length, "rollout requires padding"
-
-        self.validset = validset
     
     def fetch_goal(self, demo_id, t):
         """
@@ -70,105 +68,34 @@ class RobomimicRolloutEnv(RolloutEnv):
         
     def create_env(self):
         """
-        Create Robosuite environment.
+        Create and return Robosuite environment.
         """
         # load env metadata from training file
-        self.env_meta = json.loads(self.validset.hdf5_file["data"].attrs["env_args"])
-        self.env = EnvRobosuite(
-            env_name=self.env_meta["env_name"],
+        env_meta = json.loads(self.validset.hdf5_file["data"].attrs["env_args"])
+        return EnvRobosuite(
+            env_name=env_meta["env_name"],
             obs_key_to_modality=self.obs_key_to_modality,
             render=self.render_video,
             use_image_obs=(ObsUtils.Modality.RGB in self.obs_key_to_modality.values()),
             use_depth_obs=(ObsUtils.Modality.DEPTH in self.obs_key_to_modality.values()),
-            **self.env_meta["env_kwargs"]
+            **env_meta["env_kwargs"]
         )
 
-    def run_rollout(
-            self, 
-            policy, 
-            demo_id,
-            video_writer=None,
-            video_skip=5,
-            horizon=None,
-            terminate_on_success=False
-        ):
+    def init_demo(self, demo_id):
         """
-        Run rollout on a single demo and save stats (and video if necessary).
+        Initialize environment for demo by loading models
+        and setting simulator state. 
 
         Args:
-            policy (BC instance): policy to use for rollouts
+            demo_id (str): id of the demo, e.g., demo_0
 
-            demo_id (str): id of demo to rollout
-
-            video_writer (imageio Writer instance): if not None, use video writer object to append frames at 
-                rate given by @video_skip
-
-            video_skip (int): how often to write video frame
-
-            horizon (int): horizon of rollout episode. If None, use demo length instead.
-
-            terminate_on_success (bool): if True, terminate episode early as soon as a success is encountered
-
-        Returns:
-            results (dict): dictionary of results with the keys "horizon" and "success"
+        Returns: 
+            observation (dict): observation dictionary after initializing demo
         """
-        super(RobomimicRolloutEnv, self).run_rollout(
-            policy=policy,
-            demo_id=demo_id,
-            video_writer=video_writer, 
-            video_skip=video_skip, 
-            horizon=horizon,
-            terminate_on_success=terminate_on_success
-        )
-        
-        demo_len = self.validset.get_demo_len(demo_id=demo_id)
-        horizon = demo_len if horizon is None else horizon
-        demo_index = self.validset.demo_id_to_start_index[demo_id]
-        
-        # switch to eval mode
-        policy.eval()
-
-        # init demo
         xml = self.validset.hdf5_file[f"data/{demo_id}"].attrs["model_file"]
         init_state = self.validset.hdf5_file[f"data/{demo_id}/states"][0]
         self.env.load_env(xml)
-        obs = self.env.reset_to(init_state)
-
-        # policy inputs from initial observation
-        inputs = self.inputs_from_initial_obs(obs=obs, demo_id=demo_id)
-
-        results = {}
-        video_count = 0  # video frame counter
-        success = False
-
-        for step_i in range(horizon):
-            # compute new inputs
-            inputs = self.inputs_from_new_obs(x=inputs, obs=obs, demo_id=demo_id, t=demo_index+step_i)
-
-            # get action from policy
-            ac = policy(inputs)
-
-            # play action
-            obs = self.env.step(ac)
-
-            success = self.env.is_success()
-
-            # visualization
-            if video_writer is not None:
-                if video_count % video_skip == 0:
-                    video_img = self.env.render()
-                    video_writer.append_data(video_img)
-                video_count += 1
-
-            # break if success
-            if terminate_on_success and success:
-                break
-        
-        results["horizon"] = step_i + 1
-        results["success"] = success
-        
-        return results
-
+        return self.env.reset_to(init_state)
 
         
 
--- bc_algos/rollout/rollout_env.py
+++ bc_algos/rollout/rollout_env.py
@@ -1,3 +1,4 @@
+from bc_algos.dataset.dataset import MIMODataset
 import bc_algos.utils.tensor_utils as TensorUtils
 from bc_algos.models.policy_nets import BC
 import imageio
@@ -15,9 +16,9 @@ class RolloutEnv:
     Abstract class used to rollout policies. Inherit from this class for 
     different environments. 
     """
-
     def __init__(
-            self,  
+            self,
+            validset,  
             obs_group_to_key,
             obs_key_to_modality,
             frame_stack=0,
@@ -26,6 +27,8 @@ class RolloutEnv:
             ):
         """
         Args:
+            validset (MIMODataset): validation dataset for rollout
+
             obs_group_to_key (dict): dictionary mapping observation group to observation key
 
             obs_key_to_modality (dict): dictionary mapping observation key to modality
@@ -36,17 +39,21 @@ class RolloutEnv:
 
             render_video (bool): whether to render rollout on screen
         """
+        assert isinstance(validset, MIMODataset)
+        assert validset.pad_frame_stack and validset.pad_seq_length, "rollout requires padding"
+
+        self.validset = validset
         self.obs_group_to_key = obs_group_to_key
         self.obs_key_to_modality = obs_key_to_modality
         self.n_frame_stack = frame_stack
         self.gc = gc
         self.render_video = render_video
 
-        self.create_env()
+        self.env = self.create_env()
 
     def create_env(self):
         """
-        Create environment associated with dataset.
+        Create and return environment associated with dataset.
         """
         return NotImplementedError
     
@@ -58,7 +65,7 @@ class RolloutEnv:
         Args: 
             obs (dict): maps obs_key to data of shape [D]
 
-            demo_id (str): id of the demo, e.g., demo_0
+            demo_id: id of the demo
 
         Returns:
             x (dict): maps obs_group to obs_key to 
@@ -89,7 +96,7 @@ class RolloutEnv:
 
             obs (dict): maps obs_key to data of shape [D]
 
-            demo_id (str): id of the demo, e.g., demo_0
+            demo_id: id of the demo
 
             t (int): timestep in trajectory
 
@@ -112,12 +119,25 @@ class RolloutEnv:
         Get goal for specified demo and time if goal-conditioned.
 
         Args: 
-            demo_id (str): id of the demo, e.g., demo_0
+            demo_id: id of the demo
 
             t (int): timestep in trajectory
 
         Returns:
             goal seq np.array of shape [B=1, T=validset.n_frame_stack+1, D]
+        """
+        return NotImplementedError
+    
+    def init_demo(self, demo_id):
+        """
+        Initialize environment for demo by loading models
+        and setting simulator state. 
+
+        Args:
+            demo_id: id of the demo
+
+        Returns: 
+            observation (dict): observation dictionary after initializing demo
         """
         return NotImplementedError
 
@@ -136,7 +156,7 @@ class RolloutEnv:
         Args:
             policy (BC instance): policy to use for rollouts
 
-            demo_id (str): id of demo to rollout
+            demo_id: id of the demo to rollout
 
             video_writer (imageio Writer instance): if not None, use video writer object to append frames at 
                 rate given by @video_skip
@@ -151,6 +171,49 @@ class RolloutEnv:
             results (dict): dictionary of results with the keys "horizon" and "success"
         """
         assert isinstance(policy, BC)
+
+        demo_len = self.validset.get_demo_len(demo_id=demo_id)
+        horizon = demo_len if horizon is None else horizon
+
+        # switch to eval mode
+        policy.eval()
+
+        obs = self.init_demo()
+
+        # policy inputs from initial observation
+        inputs = self.inputs_from_initial_obs(obs=obs, demo_id=demo_id)
+
+        results = {}
+        video_count = 0  # video frame counter
+        success = False
+
+        for step_i in range(horizon):
+            # compute new inputs
+            inputs = self.inputs_from_new_obs(x=inputs, obs=obs, demo_id=demo_id, t=step_i)
+
+            # get action from policy
+            ac = policy(inputs)
+
+            # play action
+            obs = self.env.step(ac)
+
+            success = self.env.is_success()
+
+            # visualization
+            if video_writer is not None:
+                if video_count % video_skip == 0:
+                    video_img = self.env.render()
+                    video_writer.append_data(video_img)
+                video_count += 1
+
+            # break if success
+            if terminate_on_success and success:
+                break
+        
+        results["horizon"] = step_i + 1
+        results["success"] = success
+        
+        return results
 
     def rollout_with_stats(
             self, 
@@ -169,7 +232,7 @@ class RolloutEnv:
         Args:
             policy (RolloutPolicy instance): policy to use for rollouts
 
-            demo_id (str): id of demo to rollout
+            demo_id: id of the demo to rollout
 
             video_dir (str): (optional) directory to save rollout videos
 
