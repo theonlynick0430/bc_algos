830829a5f6e9955b373b13f011e5694f8540789c
Nikhil Sridhar <niksrid@gl-login2.arc-ts.umich.edu>
2024-05-03 00:21:39 -0400

    bash updates

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -15,5 +15,5 @@
 source /sw/pkgs/arc/python3.10-anaconda/2023.03/etc/profile.d/conda.sh
 conda activate bc_algos
 cd /home/niksrid/JJ/bc_algos
-python scripts/preprocess_isaac.py --config config/bc_transformer.json --dataset /nfs/turbo/coe-jjparkcv/datasets/isaac-gym-pick-place/simple/dataset_v16_diff --output /nfs/turbo/coe-jjparkcv/datasets/isaac-gym-pick-place/simple/preprocessed/dataset_v16_diff --ortho6D --world --cuda
+python scripts/preprocess_isaac.py --config config/bc_transformer.json --dataset /nfs/turbo/coe-jjparkcv/datasets/isaac-gym-pick-place/debug/[name] --output /nfs/turbo/coe-jjparkcv/datasets/isaac-gym-pick-place/debug/preprocessed/[name] --cuda
 wait
