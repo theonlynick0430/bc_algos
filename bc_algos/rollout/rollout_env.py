bd142ac86bf37adbd9fbc657e691856bd831d661
Nikhil Sridhar <niksrid@umich.edu>
2024-05-08 22:03:05 +0000

    rollout plot for world

--- bc_algos/rollout/rollout_env.py
+++ bc_algos/rollout/rollout_env.py
@@ -309,6 +309,7 @@ class RolloutEnv:
 
             # query policy for actions
             actions = self.policy(x).squeeze(0) # remove batch dim
+            pred = actions.detach().cpu().numpy()
             if self.use_ortho6D or self.use_world:
                 actions = self.postprocess_action(action=actions, obs=obs)
             actions = actions.detach().cpu().numpy()
@@ -317,7 +318,7 @@ class RolloutEnv:
             index = self.validset.index_from_timestep(demo_id=demo_id, t=step_i)
             frame = self.validset[index]
             target = frame[self.validset.action_key][self.frame_stack:, :]
-            e = np.mean(np.abs(target-actions), axis=-1)
+            e = np.mean(np.abs(target-pred), axis=-1)
             coef = np.full([e.shape[0]], 1.)
             if self.validset.get_pad_mask is True:
                 pad_mask = frame["pad_mask"][self.frame_stack:]
